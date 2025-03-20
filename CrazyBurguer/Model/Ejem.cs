00be0220bb711f54c5fe9a68d120459dc8291e1f
Priscila <bex4502@outlook.es>
2025-03-20 17:34:35 -0500

    ejem1

--- CrazyBurguer/Model/Ejem.cs
+++ CrazyBurguer/Model/Ejem.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CrazyBurguer.Controlador
+namespace CrazyBurguer.Model
 {
-    class Usuario
+    class Ejem
     {
-        
     }
 }
